3118c8b9e3289bac9a672cd93668c74b531f4b0b
Victor I. Afolabi <javafolabi@gmail.com>
2023-06-11 11:05:31 +0100

    Add argument parser

--- src/rlhf_trl/args.py
+++ src/rlhf_trl/args.py
@@ -1,76 +1,168 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
+from dataclasses import field
+
+from transformers import HfArgumentParser
 
 
 @dataclass
 class ScriptArgs:
     """The name of the Causal LM wwe wish to fine-tune with PPO."""
 
-    model_name: str = field(
+    model_name: str | None = field(
         default='',
         metadata={
             'help': 'The name of the Causal LM wwe wish to fine-tune with PPO.',
-        }
+        },
     )
 
-    tokenizer_name: str = field(
+    tokenizer_name: str | None = field(
         default='',
         metadata={
             'help': 'The name of the tokenizer to use.',
-        }
+        },
     )
 
-    reward_model_name: str = field(
+    reward_model_name: str | None = field(
         default='',
         metadata={
             'help': 'The name of the reward model to use.',
-        }
+        },
     )
 
-    log_with: str = field(
+    log_with: str | None = field(
         default='wandb',
         metadata={
             'help': 'The logger to use.',
-        }
+        },
     )
 
-    learning_rate: float = field(
+    learning_rate: float | None = field(
         default=1e-5,
         metadata={
             'help': 'The learning rate to use.',
-        }
+        },
     )
 
-    output_max_length: int = field(
+    output_max_length: int | None = field(
         default=1024,
         metadata={
             'help': 'The maximum length of the output sequence for generation.',
-        }
+        },
     )
 
-    mini_batch_size: int = field(
+    mini_batch_size: int | None = field(
         default=1,
         metadata={
             'help': 'The mini-batch size to use for PPO.',
-        }
+        },
     )
 
-    batch_size: int = field(
+    batch_size: int | None = field(
         default=32,
         metadata={
             'help': 'The batch size to use for PPO.',
-        }
+        },
     )
 
-    ppo_epochs: int = field(
+    ppo_epochs: int | None = field(
         default=4,
         metadata={
             'help': 'The number of PPO epochs.',
-        }
+        },
     )
 
-    gradient_accumulation_steps: int = field(
+    gradient_accumulation_steps: int | None = field(
         default=4,
         metadata={
             'help': 'The number of gradient accumulation steps.',
-        }
+        },
     )
+
+    adafactor: bool | None = field(
+        default=False,
+        metadata={
+            'help': 'Whether to use AdaFactor instead of AdamW.',
+        },
+    )
+
+    early_stopping: bool | None = field(
+        default=False,
+        metadata={
+            'help': 'Whether to use early stopping.',
+        },
+    )
+
+    target_kl: float | None = field(
+        default=0.1,
+        metadata={
+            'help': 'The target KL divergence for early stopping.',
+        },
+    )
+
+    reward_baseline: float | None = field(
+        default=0.0,
+        metadata={
+            'help': 'The reward baseline is a value subtracted from the reward.',
+        },
+    )
+
+    batched_gen: bool | None = field(
+        default=False,
+        metadata={
+            'help': 'Whether to use batched generation.',
+        },
+    )
+
+    save_freq: int | None = field(
+        default=None,
+        metadata={
+            'help': 'The frequency with which to save the model.',
+        },
+    )
+
+    output_dir: str | None = field(
+        default='runs/',
+        metadata={
+            'help': 'The output directory.',
+        },
+    )
+
+    seed: int | None = field(
+        default=42,
+        metadata={
+            'help': 'The seed.',
+        },
+    )
+
+    steps: int | None = field(
+        default=20_000,
+        metadata={
+            'help': 'The number of training steps.',
+        },
+    )
+
+    init_kl_coef: float | None = field(
+        default=0.2,
+        metadata={
+            'help': 'The initial KL coefficient (used for adaptive & linear control.',
+        },
+    )
+
+    adap_kl_ctrl: bool | None = field(
+        default=True,
+        metadata={
+            'help': 'Whether to adaptively control the KL coefficient, linear otherwise.',
+        },
+    )
+
+
+def parse_args() -> ScriptArgs:
+    """Parse the command line arguments.
+
+    Returns:
+        ScriptArgs: The parsed command line arguments.
+
+    """
+    parser = HfArgumentParser(ScriptArgs)
+    args = parser.parse_args_into_dataclasses()[0]
+    return args
